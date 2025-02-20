3d05d028754eb796df71e42272ba4aedfbdbdc43
Karina_Shcherbakova <karina.shcherbakova18@gmail.com>
2025-02-20 23:04:29 +0100

    Remove commented sections in eslint.config.mjs for clean code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,19 @@
-// import globals from 'globals';
-// import pluginJs from '@eslint/js';
-
-// export default [
-//   pluginJs.configs.recommended,
-//   {
-//     files: ['src/**/*.js'],
-//     languageOptions: { globals: globals.node },
-//     rules: {
-// 	    semi: 'error',
-// 	    'no-unused-vars': ['error', { args: 'none' }],
-// 	    'no-undef': 'error'
-// 	  },
-//   },
-// ];
-
-
 import globals from 'globals';
 
 export default [
   {
-    // Використовуємо стандартну конфігурацію ESLint
     extends: ['eslint:recommended'],
     parserOptions: {
-      sourceType: 'module', // Для роботи з ES-модулями
+      sourceType: 'module',
     },
-    files: ['src/**/*.js'], // Застосовуємо тільки для файлів у src
+    files: ['src/**/*.js'],
     languageOptions: {
-      globals: globals.node, // Глобальні змінні для Node.js
+      globals: globals.node,
     },
     rules: {
-      semi: 'error', // Обов'язковий кінець рядка
-      'no-unused-vars': ['error', { args: 'none' }], // Не дозволяти непотрібні аргументи
-      'no-undef': 'error', // Не використовувати невизначені змінні
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
     },
   },
 ];
